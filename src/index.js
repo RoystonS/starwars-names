9652209af0bffd980ee214088e6c3468dc53fdb8
Royston Shufflebotham <Royston.Shufflebotham@uk.ibm.com>
2017-02-03 22:19:40 +0000

    feat(random): Add ability to get an array
    New API for number

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 var uniqueRandomArray = require('unique-random-array');
 var starWarsNames = require('./starwars-names.json');
+var getRandomItem = uniqueRandomArray(starWarsNames)
 
 module.exports = {
   all: starWarsNames,
-  random: uniqueRandomArray(starWarsNames)
+  random: random
 };
+
+function random(n) {
+  if (n === undefined) {
+    return getRandomItem();
+  } else {
+    var result = [];
+    for (var i = 0; i < n; i++) {
+      result.push(getRandomItem());
+    }
+    return result;
+  }
+}
 
--- src/index.test.js
+++ src/index.test.js
@@ -23,6 +23,14 @@ describe('starwars-names', function() {
       var item = starWars.random();
       expect(starWars.all).to.include(item);
     });
+
+    it('returns an array of random items if given a count', function() {
+      var items = starWars.random(3);
+      expect(items).to.have.length(3);
+      items.forEach(function(item) {
+        expect(starWars.all).to.include(item);
+      });
+    });
   });
 });
 
